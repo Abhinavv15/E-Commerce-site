71f03f948200a55802f482fbbef94fc393396d0e
Abhinavv15 <abhinavdhanesh15@gmail.com>
2025-01-23 11:11:29 +0530

    Sign-Up post added

--- Backend/server.js
+++ Backend/server.js
@@ -65,6 +65,28 @@ app.post('/upload', upload.single('myFile'), (req, res) => {
     }
 });
 
+
+
+app.post("/signup" , async (req,res) => {
+    console.log(req.body)
+    const {name,email,password}=req.body
+    const userPresent=await userModel.findOne({email})
+    if(userPresent?.email){
+        res.send("Try loggin in ,already exist")
+    }else{
+        try {
+            bcrypt.hash(password,4,async function (err,hash){
+                const user = new userModel({name,email,password:hash})
+                await user.save()
+                res.send("Sign up successfull")
+            })
+        } catch (error) {
+            console.log(err)
+            res.send("Something went wrong,pls try again later")
+        }
+    }
+})
+
 app.listen(PORT, async () => {
     try {
         await connection;
